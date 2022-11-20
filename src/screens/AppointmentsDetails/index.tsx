dd2f4e7d5d2f6e23824474692b57039b5120d126
Lucas Ferreira <lucas.ferreira@agenciam3.com>
2022-11-20 13:15:16 -0300

    fix(signin)

--- src/screens/AppointmentsDetails/index.tsx
+++ src/screens/AppointmentsDetails/index.tsx
@@ -1,10 +1,10 @@
 import React, { ReactNode } from 'react'
 import {
-  View,
-  Text,
-  ImageBackground,
-  ImageBackgroundProps,
-  FlatList
+	View,
+	Text,
+	ImageBackground,
+	ImageBackgroundProps,
+	FlatList
 } from 'react-native'
 import { BorderlessButton } from 'react-native-gesture-handler'
 import { Fontisto } from '@expo/vector-icons'
@@ -22,82 +22,82 @@ import { ListDivider } from '../../components/ListDivider'
 import { ButtonIcon } from '../../components/ButtonIcon'
 
 type Props = ImageBackgroundProps & {
-  chilren: ReactNode
+	chilren: ReactNode
 }
 
 export function AppointmentsDetails({ chilren, ...rest }: Props) {
-  const members = [
-    {
-      id: '1',
-      username: 'Rodrigo',
-      avatar_url: 'https://github.com/rodrigorgtic.png',
-      status: 'online'
-    },
-    {
-      id: '2',
-      username: 'Rodrigo',
-      avatar_url: 'https://github.com/rodrigorgtic.png',
-      status: 'offline'
-    }
-  ]
+	const members = [
+		{
+			id: '1',
+			username: 'Rodrigo',
+			avatar_url: 'https://github.com/rodrigorgtic.png',
+			status: 'online'
+		},
+		{
+			id: '2',
+			username: 'Rodrigo',
+			avatar_url: 'https://github.com/rodrigorgtic.png',
+			status: 'offline'
+		}
+	]
 
 
-  return (
-    <Background>
-      <Header
-        title='Detalhes'
-        action={
-          <BorderlessButton>
-            <Fontisto
-              name="share"
-              size={20}
-              color={theme.colors.primary}
-            />
-          </BorderlessButton>
-        }
-      />
+	return (
+		<Background>
+			<Header
+				title='Detalhes'
+				action={
+					<BorderlessButton>
+						<Fontisto
+							name="share"
+							size={20}
+							color={theme.colors.primary}
+						/>
+					</BorderlessButton>
+				}
+			/>
 
-      <ImageBackground
-        {...rest}
-        source={BannerImg}
-        style={styles.banner}
-        resizeMode='cover'
-        key={'teste'}
+			<ImageBackground
+				{...rest}
+				source={BannerImg}
+				style={styles.banner}
+				resizeMode='cover'
+				key={'teste'}
 
-      >
+			>
 
-        <View style={styles.bannerContent}>
-          <Text style={styles.title}>
-            Lendários
-          </Text>
-          <Text style={styles.subtitle}>
-            É hoje que vamos chegar ao challenger sem perder uma partida da md10
-          </Text>
-        </View>
+				<View style={styles.bannerContent}>
+					<Text style={styles.title}>
+						Lendários
+					</Text>
+					<Text style={styles.subtitle}>
+						É hoje que vamos chegar ao challenger sem perder uma partida da md10
+					</Text>
+				</View>
 
-      </ImageBackground>
+			</ImageBackground>
 
-      <ListHeader
-        title="Jogadores"
-        subtitle="Total 3"
-      />
+			<ListHeader
+				title="Jogadores"
+				subtitle="Total 3"
+			/>
 
-      <FlatList
-        data={members}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <Member data={item} />
-        )}
-        ItemSeparatorComponent={() => (<ListDivider />)}
-        style={styles.members}
-      />
+			<FlatList
+				data={members}
+				keyExtractor={(item) => item.id}
+				renderItem={({ item }) => (
+					<Member data={item} />
+				)}
+				ItemSeparatorComponent={() => (<ListDivider isCentered />)}
+				style={styles.members}
+			/>
 
-      <View style={styles.footer}>
-        <ButtonIcon title="Entrar na partida" />
+			<View style={styles.footer}>
+				<ButtonIcon title="Entrar na partida" />
 
-      </View>
-    </Background>
-  )
+			</View>
+		</Background>
+	)
 
 
 }
